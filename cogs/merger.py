fe3c35c9278877b0a5d2d69d72ec00e2158e3f52
xu3s <okniesutrisno@gmail.com>
2022-03-20 19:55:52 +0700

    i should really read the docs and test first firs before commiting.

--- cogs/merger.py
+++ cogs/merger.py
@@ -161,7 +161,7 @@ async def reactor(bot,ctx:commands.Context,misc,saved:commands.Context=None):#py
     reactions = ['⏫','⏭️']
     state = f'Current folder: {misc["folder"]}\nCurrent list:{misc["sl"]}\nimage: {d}'
     embed = discord.Embed(title='image selector',description=state)
-    embed.set_image(url=parse.quote(misc['url']))
+    embed.set_image(url=parse.quote(misc['url'],safe=':/'))
     instructions = 'react with ⏫ = add to stitch, ⏭️= add to next list.\ntype "stop" to abort. type "skip" to skip folder.'
     embed.set_footer(text=instructions)
 
